4192f57a4d22445ea3e1f263c5081d28c9b4fed6
S_hungry <1335138813@qq.com>
2019-12-16 10:31:55 +0800

    add sceneElements

---
+++ NaturalEngine/DrawableObjects/sceneElements.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef SCENELEMENTS_H
+#define SCENELEMENTS_H
+
+#include <Camera.h>
+#include <glm/glm.hpp>
+#include "Engine/buffer.h"
+#include <random>
+
+struct sceneElements
+{
+	glm::vec3 lightPos, lightColor, lightDir, fogColor, seed;
+	glm::mat4 projMatrix;
+	Camera* cam;
+	FrameBufferObject * sceneFBO;
+	bool wireframe = false;
+
+};
+
+
+
+#endif // !SCENELEMENTS_H
